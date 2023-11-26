97593e8c5b2527c155e2cc48c8e8c981ab950678
Vicky Gupta <vickygupta16599@gmail.com>
2023-11-26 12:25:20 +0530

    Added email

--- src/components/personal.js
+++ src/components/personal.js
@@ -3,17 +3,27 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
 import VG_3 from "../images/VG_3.png";
+import Table from 'react-bootstrap/Table';
+import "../App.css";
 
 export function Personal() {
   return (
     <div id="#personal" className="text-center">
-      <h3>Personal</h3>
       <Container>
         <Row>
           <Col>
             <Image src={VG_3} rounded className="front-image" />
           </Col>
-          <Col>2</Col>
+          <Col>
+            <Table>
+              <tbody>
+                <tr>
+                  <td>Email ID</td>
+                  <td className="mail-link"><a href="mailto:vickygupta16599@gmail.com">vickygupta16599@gmail.com</a></td>
+                </tr>
+              </tbody>
+            </Table>
+          </Col>
         </Row>
       </Container>
     </div>
